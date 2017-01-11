9ebd77395cf89734419b528173cc4605a95d5d72
AdamRakaska <arakaska@gmail.com>
2017-01-11 09:25:01 -0700

    Initial project skeleton check in

---
+++ GNFSCore/NullMatrix.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace GNFSCore
+{
+	public class NullMatrix
+	{
+
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# GNFS
-C# General Number Field Sieve for factoring large semi-primes
+# C# GNFS
+
+C# .NET implementation of the general number field sieve for factoring large semi-primes including polynomial selection, relations collection, and line sieving.
+
