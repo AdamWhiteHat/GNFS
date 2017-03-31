9b5c9a016d181441233a5d0a9af4c30b55386284
AdamRakaska <arakaska@gmail.com>
2017-03-30 22:20:19 -0600

    Added QuadraticResidue class

---
+++ GNFSCore/Core/QuadraticResidue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace GNFSCore
+{
+	public class QuadraticResidue
+	{
+		// a^(p-1)/2 ≡ 1 (mod p)
+		public static bool IsQuadraticResidue(BigInteger a, BigInteger p)
+		{
+			BigInteger quotient = BigInteger.Divide(p - 1, 2);
+			BigInteger modPow = BigInteger.ModPow(a, quotient, p);
+
+			return modPow.IsOne;
+		}
+	}
+}
