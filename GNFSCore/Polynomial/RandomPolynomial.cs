c8e1a8e923a072b146fd8581424f3e295ca2db24
AdamRakaska <adamwhitehat@outlook.com>
2018-04-06 02:16:41 -0600

    Added RandomPolynomial class

---
+++ GNFSCore/Polynomial/RandomPolynomial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GNFSCore.Polynomial
+{
+	using Internal;
+	public class RandomPolynomial
+	{
+		public int Degree { get; private set; }
+		public BigInteger[] Terms { get; private set; }
+
+		public BigInteger MinimumCoefficentValue { get; private set; }
+		public BigInteger MaximumCoefficentValue { get; private set; }
+
+		public RandomPolynomial(int degree, BigInteger minimumCoefficentValue, BigInteger maximumCoefficentValue)
+		{
+			Degree = degree;
+			MinimumCoefficentValue = minimumCoefficentValue;
+			MaximumCoefficentValue = maximumCoefficentValue;
+			Terms = Enumerable.Repeat(BigInteger.Zero, Degree + 1).ToArray();
+
+			RandomizeCoefficients();		
+		}
+
+		public void RandomizeCoefficients()
+		{
+			int index = 0;
+			while (index <= Degree)
+			{
+				Terms[index] = StaticRandom.NextBigInteger(MinimumCoefficentValue, MaximumCoefficentValue);
+			}
+		}
+
+		public BigInteger Evaluate(BigInteger baseM)
+		{
+			return CommonPolynomial.Evaluate(Degree, Terms, baseM);
+		}
+	}
+}
