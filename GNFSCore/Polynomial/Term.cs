1fe38dee546cfbcdc640c6db3e8f7879b3240917
AdamRakaska <adamwhitehat@outlook.com>
2018-04-08 19:29:59 -0600

    Added Term.cs, as the project file was referencing it.

---
+++ GNFSCore/Polynomial/Term.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace GNFSCore.Polynomial
+{
+	using Internal;
+
+	public class Term
+	{
+		public BigInteger Coefficient { get; set; }
+		public char Variable { get; set; }
+		public int Degree { get; set; }
+
+		public BigInteger Evaluate(BigInteger variableValue)
+		{
+			return BigInteger.Multiply(Coefficient, BigInteger.Pow(variableValue, Degree));
+		}
+
+		public override string ToString()
+		{
+			return $"{Coefficient} * {Variable}^{Degree}";
+		}
+	}
+}
